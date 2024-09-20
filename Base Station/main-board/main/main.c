7746a11e2827bcee34df07e8ad714475d7c41a2c
Steven Huang <shuang202001@gmail.com>
2024-09-19 21:49:20 -0400

    main board setup

---
+++ Base Station/main-board/main/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+
+/* Bluetooth LE Libraries, make sure BT is enabled*/
+#include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gatt_common_api.h"
+#include "sdkconfig.h"
+
+/* WiFi Libraries*/
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_netif_net_stack.h"
+#include "esp_netif.h"
+#include "nvs_flash.h"
+#include "lwip/inet.h"
+#include "lwip/netdb.h"
+#include "lwip/sockets.h"
+#if IP_NAPT
+#include "lwip/lwip_napt.h"
+#endif
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+void app_main(void)
+{
+    printf("100\n");
+}
